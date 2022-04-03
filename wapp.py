df6fcc6efe4edaa146e0ea1baefc7d3dda7a1aa3
TareqRafed <tareq.rafed22@gmail.com>
2022-04-03 23:50:09 +0300

    edition

--- wapp.py
+++ wapp.py
@@ -1,13 +1,23 @@
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 
 
 def get_last_rec():
     lastUsers = []
     driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
     driver.get("http://web.whatsapp.com")
-    source = driver.page_source
-    
+    delay = 3 # seconds
+    try:
+        myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'landing-title')))
+        if myElem:
+            print("Please sign in using your phone")
+        else:
+            print("signed in")
+    except TimeoutException:
+        print("Loading took too much time!")
     #driver.quit() 
 
